49e9fbc51e30ccd0675c608177bddd538b1d1a55
facuhump2409 <facu2409@hotmail.com>
2021-01-31 18:03:59 -0300

    Arreglado para que se agrupe por productos para que se escriba bien la cantidad y tambien arreglado para que escriba en la columna correcta usando el id de pedido

--- parser.py
+++ parser.py
@@ -12,7 +12,6 @@ clients_folder = orders_folder_year + "/Generador de pedidos/Pedidos"
 orders_generator = load_workbook(orders_folder_year + "/Generador de pedidos/Generador de pedidos.xlsx")
 
 consolidated_orders = load_workbook("Pedidos/2021/012020/Pedidos consolidados 01-2021.xlsx", data_only=True)
-client_order = load_workbook(clients_folder + "/Planilla Cliente.xlsx", data_only=True)
 prices = consolidated_orders['Precios y Menú']
 
 # costo_de_envio = 0
@@ -42,70 +41,82 @@ class Coordinate:
         self.y = self.y + 1
 
 
-def get_free_space_cell(cell, worksheet):
+def get_free_space_cell(cell, worksheet,order_id):
     # TODO ver tema de si ya tiene un numero que se frene en esa posicion
     current_value = cell.value
     coordinate = Coordinate(cell.row, cell.column)
-    while current_value is not None:
+    while current_value is not None and current_value != order_id:
         coordinate.increment_column()
         current_value = worksheet.cell(coordinate.x, coordinate.y).value
     return coordinate
 
 
-def set_value_in_empty_space(excel_file, name, value):
+def set_value_in_empty_space(excel_file, name, value,order_id):
     try:
         worksheet = excel_file['Pedido']
     except:
         worksheet = excel_file['Pedido Pency']
     for row in worksheet.iter_rows():
         for cell in row:
-            if (cell.value != None and isinstance(cell.value,
-                                                  str) and cell.value.upper() == name.upper().strip()):  # No sea Null, sea string y sea igual al nombre del valor
-                coordinate = get_free_space_cell(cell, worksheet)
-                # chr(ord('a') + 1) # para conseguir proximo caracter
-                # cell.row -> te devuelve la fila
-                # cell.column -> te devuelve la columna
-                # Si es None es que no hay nada, habria que conseguir la proxima letra del abecedario de esa columna que esta vacia
-                worksheet.cell(coordinate.x, coordinate.y).value = value
+            if (cell.value != None and isinstance(cell.value, str)): # No sea Null y sea string
+                if cell.value.upper() == "PEDIDO":
+                    free_coordinate = get_free_space_cell(cell, worksheet,order_id)
+                if cell.value.upper() == name.upper().strip():
+                    coordinate = Coordinate(cell.row,free_coordinate.y)
+                    # coordinate = get_free_space_cell(cell, worksheet)
+                    # chr(ord('a') + 1) # para conseguir proximo caracter
+                    # cell.row -> te devuelve la fila
+                    # cell.column -> te devuelve la columna
+                    # Si es None es que no hay nada, habria que conseguir la proxima letra del abecedario de esa columna que esta vacia
+                    actual_value = worksheet.cell(coordinate.x, coordinate.y).value or 0
+                    if actual_value != 0:
+                        worksheet.cell(coordinate.x, coordinate.y).value = int(value) + int(actual_value)
+                    else:
+                        worksheet.cell(coordinate.x, coordinate.y).value = value
+                    return
 
 
-def set_excel_attributes(excel_file, excel_dictionary):
+
+def set_excel_attributes(excel_file, excel_dictionary,order_id):
     for key, value in excel_dictionary.items():
-        set_value_in_empty_space(excel_file, key, value)
+        set_value_in_empty_space(excel_file, key, value,order_id)
 
 
-def set_products_to_excel(excel_file, products):
+def set_products_to_excel(excel_file, products,order_id):
     for product in products:
-        set_value_in_empty_space(excel_file, product.name, product.get_qty())
+        set_value_in_empty_space(excel_file, product.name, product.get_qty(),order_id)
 
 
 message_row = 0
 
 
 def add_atributes_to_excels(excels, excel_dictionary, order_products):
+    order_id = excel_dictionary['Pedido']
     for excel in excels:
-        set_excel_attributes(excel, excel_dictionary)
-        set_products_to_excel(excel, order_products)
+        set_excel_attributes(excel, excel_dictionary,order_id)
+        set_products_to_excel(excel, order_products,order_id)
 
 
 def get_tipo_de_envio(tipos_de_envios, texto):
     for tipo_de_envio in tipos_de_envios:
         if tipo_de_envio.upper() in texto.upper():
             return tipo_de_envio
-    return 'Programado' #Es el valor default
+    return 'Programado'  # Es el valor default
 
 
 for message in messages.iter_rows(values_only=True):
     message_row += 1
     if (message[0] == 'Si' or message[0] == 'Procesado'): continue  # or message[1] == None
+    client_order = load_workbook(clients_folder + "/Planilla Cliente.xlsx", data_only=True)
     excel_dictionary = {}
     order = message[1]
     excel_dictionary['Fecha de pedido'] = date.today().strftime("%d/%m/%Y")
-    excel_dictionary['Tipo de envío'] = get_tipo_de_envio(tipos_de_envios,re.search("(?i)(?<=Fecha de entrega: ).+", order)[0])
+    excel_dictionary['Tipo de envío'] = get_tipo_de_envio(tipos_de_envios,
+                                                          re.search("(?i)(?<=Fecha de entrega: ).+", order)[0])
     # total = re.search("(?<=Total\: ).+",order)[0]
     excel_dictionary['Pedido'] = re.search("(?i)(?<=Pedido: ).+", order)[0]
     excel_dictionary['Rango Horario'] = "16 a 20hs"  # Para 24 hs
-    excel_dictionary['Direccion'] = re.search("(?i)(?<=Direcci.n de entrega: ).+", order)[0]
+    excel_dictionary['Dirección'] = re.search("(?i)(?<=Direcci.n de entrega: ).+", order)[0]
     payment_method = re.search("(?i)(?<=Forma de pago: ).+", order)[0].strip()
     excel_dictionary['Medio de pago'] = re.sub("\(.+\)", "", payment_method).strip()
     excel_dictionary['Cliente'] = re.search("(?i)(?<=Nombre y Apellido: ).+", order)[0]
@@ -132,15 +143,16 @@ for message in messages.iter_rows(values_only=True):
             guarnicion_qty = re.search("X\d", guarnicion)
             final_qty = 1 if guarnicion_qty == None else int(re.sub("X", "", guarnicion_qty[0]).strip())
             order_products.append(Plato(final_qty * qty, nombre))
-        # items_dictionary[product] = qty
     # excel_dictionary['Zona'] = re.search("(?i)(?<=Zona de entrega: ).+",order)[0]
     messages.cell(message_row, 1).value = "Si"
     add_atributes_to_excels([client_order, consolidated_orders], excel_dictionary, order_products)
-    # set_excel_attributes(client_order, excel_dictionary)
-    # set_excel_attributes(client_order, order_products)
     client_order.save(excel_dictionary['Pedido'] + excel_dictionary['Cliente'] + ".xlsx")
-    # Setear en Si el procesado
+    client_order.close()
 orders_generator.save(orders_folder_year + "/Generador de pedidos/Generador de pedidos.xlsx")
 consolidated_orders.save("Pedidos/2021/012020/Pedidos consolidados 01-2021.xlsx")
-# orders_generator.save("26-12 Planilla de pedidos.xlsx")
-# def work_excel
+
+# TODO ver tema de que escribe el mismo archivo entonces termina escribiendo mal al cliente, y agrupar por productos para evitar que se escriba "Papas" como distintos pedidos
+# Que en el mismo pedido se haga primero una agrupacion por productos
+# Terminar de testear si escribe en la siguiente columna, hacerlo por partes para que no se confunda con los otros errores
+# Probable que dentro del for se abra la planilla del cliente de nuevo
+# Buscar como cerrar los archivos
